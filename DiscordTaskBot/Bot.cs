6af4b384cedba2ec9b6425b04a41869d3c34628f
piotrkossa <135640588+piotrkossa@users.noreply.github.com>
2025-06-03 22:04:23 +0200

    Added Discord.Net package and Bot class

---
+++ DiscordTaskBot/Bot.cs
@@ -0,0 +1,9 @@
+namespace DiscordTaskBot
+{
+    public class Bot
+    {
+        public async Task RunAsync() {
+            await Task.Delay(-1);
+        }
+    }
+}
--- DiscordTaskBot/Program.cs
+++ DiscordTaskBot/Program.cs
@@ -3,4 +3,14 @@ using System.IO;
 
 namespace DiscordTaskBot
 {
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Env.LoadEnv(".env");
+
+            var bot = new Bot();
+            await bot.RunAsync();
+        }
+    }
 }
